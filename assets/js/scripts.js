e5e9657f998c4fbf1887bec116e0b2a8abcb85d8
dzCazador <ctestaseca@hotmail.com>
2024-09-05 20:55:50 -0300

    Se agrego importar de Json y exportar JSon

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,18 +1,32 @@
 function ImprimirDatosConsola(event) {
-    // Evitar que el formulario se envíe
-    event.preventDefault();
+    const form = document.getElementById('registro');
+    //Valido Campos Obligatorios
+    if (form.checkValidity()) {
+        // Evitar que el formulario se envíe
+        event.preventDefault(); 
 
-    // Obtener los valores de los campos del formulario
-    const nombre = document.getElementById('nombre').value;
-    const apellido = document.getElementById('apellido').value;
-    const direccion = document.getElementById('direccion').value;
-    const equipo = document.getElementById('equipo').options[document.getElementById('equipo').selectedIndex].text;
+        // Obtener los valores de los campos del formulario
+        const nombre = document.getElementById('nombre').value;
+        const apellido = document.getElementById('apellido').value;
+        const email = document.getElementById('email').value;
+        const contrasenia = document.getElementById('contrasenia').value;
+        const equipo = document.getElementById('equipo').options[document.getElementById('equipo').selectedIndex].text;
 
-    // Imprimir los valores en la consola
-    console.log("Nombre: " + nombre);
-    console.log("Apellido: " + apellido);
-    console.log("Dirección: " + direccion);
-    console.log("Hincha de: " + equipo);
+        const formData = {
+            nombre: nombre,
+            apellido: apellido,
+            equipo: equipo,
+            email: email,
+            contrasenia: contrasenia
+        };
+
+        // Imprimir el objeto JSON en la consola
+        console.log(formData);
+    }
+}
+
+function cambioEquipo(equipo){
+    document.getElementById('info').innerHTML=(equipo=='boca'?'Murió en Madrid':'');
 }
 
 
@@ -24,3 +38,24 @@ function LimpiarDatos() {
     // Reinicia todos los campos del formulario
     form.reset();
 }
+
+  // Function to populate the select element
+  function cargarEquipo(jsonData) {
+    const selectElement = document.getElementById('equipo');
+    jsonData.teams.forEach(team => {
+        const option = document.createElement('option');
+        option.value = team.value;
+        option.textContent = team.label;
+        selectElement.appendChild(option);
+    });
+}
+
+// Fetch the JSON data from the external file
+fetch('/assets/docs/teams.json')
+    .then(response => response.json())
+    .then(data => {
+        cargarEquipo(data);
+    })
+    .catch(error => {
+        console.error('Error al Cargar Equipos:', error);
+    });
